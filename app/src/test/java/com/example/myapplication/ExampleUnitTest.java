184d2c73966846d3065da79a37acc502825b8c75
Alex <alexbabaeff42@gmail.com>
2021-03-30 16:54:24 -0700

    converted more stuff to java

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,8 +1,10 @@
-package com.example.myapplication
+package com.example.myapplication;
 
-import org.junit.Test
+import org.junit.Test;
 
-import org.junit.Assert.*
+import org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +13,7 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
