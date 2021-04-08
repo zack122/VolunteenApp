40eaced9849d0da704e7929d017c25b9ce69fc2c
Alex <alexbabaeff42@gmail.com>
2021-04-07 22:38:36 -0700

    started on create a post page

--- app/src/main/java/com/example/myapplication/CreatePostActivity.java
+++ app/src/main/java/com/example/myapplication/CreatePostActivity.java
@@ -3,9 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class CreatePostActivity extends AppCompatActivity {
 
@@ -13,6 +18,21 @@ public class CreatePostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        TextView createOpportunityTextView = findViewById(R.id.createOpportunityTextView);
+
+        TextPaint paint = createOpportunityTextView.getPaint();
+        float width = paint.measureText("Create Opportunity");
+
+        Shader textshader = new LinearGradient(0,0, 0, createOpportunityTextView.getTextSize()*2 ,
+                new int[]{
+                        Color.parseColor("#0062F4"),
+                        Color.parseColor("#327A26"),
+                }, null, Shader.TileMode.CLAMP);
+        createOpportunityTextView.getPaint().setShader(textshader);
+
+
+
 
 
     }
