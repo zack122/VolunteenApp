4ad9a26ae474e786cc24ef1645fac222bb31fa40
Alex Babaeff <alexbabaeff42@gmail.com>
2021-04-16 11:35:55 -0700

    Added buttons for clicked and non clicked and started functionality

--- app/src/main/java/com/example/myapplication/CreatePostActivity.java
+++ app/src/main/java/com/example/myapplication/CreatePostActivity.java
@@ -18,31 +18,111 @@ public class CreatePostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+        //connect XML views
+        ImageButton environment = findViewById(R.id.category_environment);
+        ImageButton crafting = findViewById(R.id.category_crafting);
+        ImageButton fundraiser = findViewById(R.id.category_fundraisers);
+        ImageButton cleaning = findViewById(R.id.category_cleaning);
+        ImageButton sports = findViewById(R.id.categories_sports);
+        ImageButton recycling = findViewById(R.id.categories_recycling);
 
-        TextView stepOneTextView = findViewById(R.id.stepOneText);
-        TextView stepTwoTextView = findViewById(R.id.stepTwoText);
-        TextView stepThreeTextView = findViewById(R.id.stepThreeText);
-        TextView stepFourTextView = findViewById(R.id.stepFourText);
-        TextView stepFiveTextView = findViewById(R.id.stepFiveText);
-        TextView stepSixTextView = findViewById(R.id.stepSixText);
-        TextView stepSevenTextView = findViewById(R.id.stepSevenText);
 
 
-        TextPaint paint = stepOneTextView.getPaint();
-        float width = paint.measureText("Create Opportunity");
 
-        Shader textshader = new LinearGradient(0,0, 0, 90 ,
-                new int[]{
-                        Color.parseColor("#0062F4"),
-                        Color.parseColor("#327A26"),
-                }, null, Shader.TileMode.CLAMP);
-        stepOneTextView.getPaint().setShader(textshader);
-        stepTwoTextView.getPaint().setShader(textshader);
-        stepThreeTextView.getPaint().setShader(textshader);
-        stepFourTextView.getPaint().setShader(textshader);
-        stepFiveTextView.getPaint().setShader(textshader);
-        stepSixTextView.getPaint().setShader(textshader);
-        stepSevenTextView.getPaint().setShader(textshader);
+        //set up button functions
+        environment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set to be clicked
+                environment.setImageResource(R.drawable.category_environment_clicked);
+                //set the others to be unclicked
+
+                crafting.setImageResource(R.drawable.category_crafting);
+                fundraiser.setImageResource(R.drawable.category_fundraisers);
+                cleaning.setImageResource(R.drawable.category_cleaning);
+                sports.setImageResource(R.drawable.category_sports);
+                recycling.setImageResource(R.drawable.category_recycling);
+
+            }
+        });
+
+
+
+        crafting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crafting.setImageResource(R.drawable.category_crafting_clicked);
+                //set the others to be unclicked
+                environment.setImageResource(R.drawable.category_environment);
+                fundraiser.setImageResource(R.drawable.category_fundraisers);
+                cleaning.setImageResource(R.drawable.category_cleaning);
+                sports.setImageResource(R.drawable.category_sports);
+                recycling.setImageResource(R.drawable.category_recycling);
+            }
+        });
+
+
+
+        fundraiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fundraiser.setImageResource(R.drawable.category_fundraisers_clicked);
+                //set the others to be unclicked
+                environment.setImageResource(R.drawable.category_environment);
+                crafting.setImageResource(R.drawable.category_crafting);
+                cleaning.setImageResource(R.drawable.category_cleaning);
+                sports.setImageResource(R.drawable.category_sports);
+                recycling.setImageResource(R.drawable.category_recycling);
+            }
+        });
+
+
+
+        cleaning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleaning.setImageResource(R.drawable.category_cleaning_clicked);
+                //set the others to be unclicked
+                environment.setImageResource(R.drawable.category_environment);
+                crafting.setImageResource(R.drawable.category_crafting);
+                fundraiser.setImageResource(R.drawable.category_fundraisers);
+                sports.setImageResource(R.drawable.category_sports);
+                recycling.setImageResource(R.drawable.category_recycling);
+            }
+        });
+
+
+
+        sports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sports.setImageResource(R.drawable.category_sports_clicked);
+                //set the others to be unclicked
+                environment.setImageResource(R.drawable.category_environment);
+                crafting.setImageResource(R.drawable.category_crafting);
+                fundraiser.setImageResource(R.drawable.category_fundraisers);
+                cleaning.setImageResource(R.drawable.category_cleaning);
+                recycling.setImageResource(R.drawable.category_recycling);
+            }
+        });
+
+
+
+        recycling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recycling.setImageResource(R.drawable.category_recycling_clicked);
+                //set the others to be unclicked
+                environment.setImageResource(R.drawable.category_environment);
+                crafting.setImageResource(R.drawable.category_crafting);
+                fundraiser.setImageResource(R.drawable.category_fundraisers);
+                cleaning.setImageResource(R.drawable.category_cleaning);
+                sports.setImageResource(R.drawable.category_sports);
+
+
+            }
+        });
+
 
 
 
